56e5dbad828775ed52cf09cf582958da29b94730
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-11-15 07:44:19 +0200

    Delete unnecessary files, add linters, instal redux, react-icons and install styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
 
 export default App;
+// https://documenter.getpostman.com/view/10831675/SzYZ1eNY#cb5ec096-a897-49dd-a4a1-e27fe32605df
